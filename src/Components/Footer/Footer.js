98067f4c7eff7d8c5bcf15071e2086e7d571bb71
maryam <socialmari3@gmail.com>
2022-04-11 22:50:06 +0300

    Edit: Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,13 @@
 import React from "react";
-import './Footer.css';
+import "./Footer.css";
+import popcorn from "./popcorn.png";
+
 export default function Footer() {
-  return <div className="footer">Enjoy your movie!</div>;
+  return (
+    <div className="footer">
+      <div className="footerTitle">Movie App</div>
+      <img src={popcorn} alt="" className="footerImg" />
+      <div>Enjoy!</div>
+    </div>
+  );
 }
