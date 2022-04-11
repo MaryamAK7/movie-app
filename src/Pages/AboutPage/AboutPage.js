ed6e6781e55b69460b266b857d0d0f2faec9061a
maryam <socialmari3@gmail.com>
2022-04-11 18:49:33 +0300

    Add : AboutPage

--- src/Pages/AboutPage/AboutPage.js
+++ src/Pages/AboutPage/AboutPage.js
@@ -4,10 +4,12 @@ import './AboutPage.css';
 export default function AboutPage() {
   return (
     <div className="about">
-      This is my Movie Application
+      The Movie App allows you to check the trending movies daily! You can also search for any specific movie. <br/> It's build using the Movie database Api. 
       <br />
-      Hope You like it!
-      <br /> Maryam
+      <div className="aboutDesc">Languages used : HTML, React, CSS </div>
+      <div className="enjoyAbout"> Enjoy! </div>
+     
+     
     </div>
   );
 }
