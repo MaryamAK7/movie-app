d60cc1429cf90d6acf74dc677ee9c25416657783
maryam <socialmari3@gmail.com>
2022-04-11 23:03:32 +0300

    Edit img

--- src/Pages/AboutPage/AboutPage.js
+++ src/Pages/AboutPage/AboutPage.js
@@ -1,15 +1,19 @@
 import React from "react";
-import './AboutPage.css';
+import "./AboutPage.css";
+import pop from './popcorn.png';
 
 export default function AboutPage() {
   return (
-    <div className="about">
-      The Movie App allows you to check the trending movies daily! You can also search for any specific movie. <br/> It's build using the Movie database Api. 
-      <br />
-      <div className="aboutDesc">Languages used : HTML, React, CSS </div>
-      <div className="enjoyAbout"> Enjoy! </div>
-     
-     
+    <div className="AboutPage">
+      <img src={pop} alt='' className="aboutImg"/>
+      <div className="about">
+        The Movie App allows you to check the trending movies daily! You can
+        also search for any specific movie. <br /> It's build using the Movie
+        database Api.
+        <br />
+        <div className="aboutDesc">Languages used : HTML, React, CSS </div>
+        <div className="enjoyAbout"> Enjoy! </div>
+      </div>
     </div>
   );
 }
