1ff79ce50d3e78302ef9f090b678bb9c30976826
maryam <socialmari3@gmail.com>
2022-04-15 13:14:49 +0300

    Fix: broken link

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -14,7 +14,7 @@ export default function NavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/About/">About</Nav.Link>
+            <Nav.Link href="/about/">About</Nav.Link>
           </Nav>
           <div inline>
             <SearchBox />
